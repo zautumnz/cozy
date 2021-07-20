89a344c1ddcf2a33900cd70e9e6ff547d142c9c2
zacanger <zac@zacanger.com>
2021-07-20 00:05:05 -0600

    feat: ctags config

--- editor/README.md
+++ editor/README.md
@@ -1,5 +1,6 @@
 Support for editors and other tools.
 
 * ./cozy.cloc is a langdef for [cloc](https://github.com/AlDanial/cloc).
-* ./cozy.el is a mode for Emacs
-* ./vim is a WIP full plugin for vim support
+* ./cozy.el is a mode for Emacs, mostly unmaintained
+* ./vim is a full plugin for vim support
+* ./ctags is a WIP addition to ~/.ctags
---
+++ editor/cozy.el
@@ -0,0 +1,93 @@
+;;; cozy.el --- mode for editing cozy scripts
+
+;; Copyright (C) 2018 Steve Kemp
+
+;; Author: Steve Kemp <steve@steve.fi>
+;; Keywords: languages
+;; Version: 1.0
+
+;;; Commentary:
+
+;; Provides support for editing cozy scripts with full support for
+;; font-locking, but no special keybindings, or indentation handling.
+
+;;;; Enabling:
+
+;; Add the following to your .emacs file
+
+;; (require 'cozy)
+;; (setq auto-mode-alist (append '(("\\.mon$" . cozy-mode)) auto-mode-alist)))
+
+
+
+;;; Code:
+
+(defvar cozy-constants
+  '("true"
+    "false"))
+
+(defvar cozy-keywords
+  '(
+    "else"
+    "fn"
+    "for"
+    "foreach"
+    "if"
+    "in"
+    "let"
+    "mutable"
+    "return"
+    ))
+
+;; The language-core and functions from the standard-library.
+(defvar cozy-functions
+  '(
+    "args"
+    "doc"
+    "error"
+    "exit"
+    "file"
+    "first"
+    "import"
+    "int"
+    "last"
+    "len"
+    "macro"
+    "match"
+    "math"
+    "print"
+    "quote"
+    "read"
+    "rest"
+    "set"
+    "string"
+    "type"
+    "unquote"
+    "version"
+    "while"
+    ))
+
+
+(defvar cozy-font-lock-defaults
+  `((
+     ("\"\\.\\*\\?" . font-lock-string-face)
+     (";\\|,\\|=" . font-lock-keyword-face)
+     ( ,(regexp-opt cozy-keywords 'words) . font-lock-builtin-face)
+     ( ,(regexp-opt cozy-constants 'words) . font-lock-constant-face)
+     ( ,(regexp-opt cozy-functions 'words) . font-lock-function-name-face)
+     )))
+
+(define-derived-mode cozy-mode fundamental-mode "cozy script"
+  "cozy-mode is a major mode for editing cozy scripts"
+  (setq font-lock-defaults cozy-font-lock-defaults)
+
+  ;; Comment handler for single & multi-line modes
+  (modify-syntax-entry ?\/ ". 124b" cozy-mode-syntax-table)
+  (modify-syntax-entry ?\* ". 23n" cozy-mode-syntax-table)
+
+  ;; Comment ender for single-line comments.
+  (modify-syntax-entry ?\n "> b" cozy-mode-syntax-table)
+  (modify-syntax-entry ?\r "> b" cozy-mode-syntax-table)
+  )
+
+(provide 'cozy)
