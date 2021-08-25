1ad24c0d05f8b903a9da2ba08b4140be461d12ec
zacanger <zac@zacanger.com>
2021-08-25 16:13:02 -0600

    chore: add a test for history size

---
+++ repl/repl_test.go
@@ -0,0 +1,18 @@
+package repl
+
+import (
+	"os"
+	"testing"
+)
+
+func TestGetHistorySize(t *testing.T) {
+	v1 := getHistorySize()
+	if v1 != 1000 {
+		t.Fatalf("expected default history size to be 1000")
+	}
+	os.Setenv("COZY_HISTSIZE", "500")
+	v2 := getHistorySize()
+	if v2 != 500 {
+		t.Fatalf("excpted history size to be 500")
+	}
+}
